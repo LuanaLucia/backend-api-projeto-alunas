45ce457470701a5ee99ee5bf71098c689e73cb7f
LuanaLucia <luanasantos@outlook.com>
2023-05-12 23:30:16 -0300

    ajuste

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
+app.use(cors());
+  // Sinalizar uso de Json
+app.use(express.json());
 
 // Sinalizar uso de Json
 app.use(express.json())
